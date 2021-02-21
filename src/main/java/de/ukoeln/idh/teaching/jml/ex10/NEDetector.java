228bcf8c7ebdfd70c49aa1fbcec758b123b87c21
Cosima Granderath-Miegel <cgrander@smail.uni-koeln.de>
2021-02-21 14:28:53 +0100

    bug fixes

--- src/main/java/de/ukoeln/idh/teaching/jml/ex10/NEDetector.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex10/NEDetector.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 
+
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.ukoeln.idh.teaching.jml.ex10.types.NamedEntity;
@@ -23,25 +24,31 @@ public class NEDetector extends JCasAnnotator_ImplBase{
                     firstWord= false;
                     continue;
                 }
-                if(Character.isUpperCase(t.getCoveredText().charAt(0))){
+                else if(Character.isUpperCase(t.getCoveredText().charAt(0)) && (t.getEnd()==t.getBegin()+1)){
+                    /*Filters out I but other stopwords still present */
+                    continue;
+                }else if(Character.isUpperCase(t.getCoveredText().charAt(0)) && Character.isUpperCase(t.getCoveredText().charAt(1))){
+                    continue;
+                }else if(Character.isUpperCase(t.getCoveredText().charAt(0))){
                     int startIndex = t.getBegin();
+                    
                     int endIndex = endOfEntity(t, s, jcas);
+                    
                     NamedEntity ne = new NamedEntity(jcas, startIndex, endIndex);
                     ne.addToIndexes();
                 }
+                
             }
 
         }
     }
 
     public int endOfEntity (Token t, Sentence s, JCas jcas) {
-        /*Not sure whether this starts at correct poition
-        * but since I cannot test this
-        * I just hope it works*/
-        List<Token> sentenceTokens = jcas.select(Token.class).coveredBy(s).following(t.getEnd()).asList();
-        
+   
+        List<Token> sentenceTokens = jcas.select(Token.class).following(t.getEnd()-1).asList();
         
         if(Character.isUpperCase(sentenceTokens.get(0).getCoveredText().charAt(0))){
+ 
             return endOfEntity(sentenceTokens.get(0), s, jcas);     
         }
         return t.getEnd();
